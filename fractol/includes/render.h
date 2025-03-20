caa0072f8702c729877753f71bf6ab842ee1c089
deniscrafts <denis.r.d073@gmail.com>
2025-03-20 20:41:30 +0100

    First commit

---
+++ fractol/includes/render.h
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   render.h                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: denrodri <denrodri@student.42malaga.com>   +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/03/12 19:28:00 by denrodri          #+#    #+#             */
+/*   Updated: 2025/03/12 19:56:52 by denrodri         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+
+#ifndef RENDER_H
+# define RENDER_H
+
+# include "fractal.h"
+
+void	set_pixel_color(t_engine *engine, int x, int y, int color);
+void	change_color(int key, t_engine *engine);
+void	change_view(int key, t_engine *engine);
+int		calc_fractal(t_fractal *fract, t_complex *c, int x, int y);
+void	draw_fractal(t_engine *engine);
+
+#endif  /* RENDER_H */
